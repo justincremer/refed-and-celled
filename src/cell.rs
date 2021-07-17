3a818d2fdc08c4980575f17518d580f7ee7f60ff
justincremer <jacremer@live.com>
2021-07-17 01:52:10 -0700

    intial

--- src/cell.rs
+++ src/cell.rs
@@ -1,3 +1,44 @@
+use std::cell::UnsafeCell;
+
+#[derive(Debug)]
 pub struct Cell<T> {
-    inner: *mut T,
+    inner: UnsafeCell<T>,
+}
+
+impl<T> Cell<T> {
+    pub fn new(inner: T) -> Self {
+        Cell {
+            inner: UnsafeCell::new(inner),
+        }
+    }
+
+    pub fn get(&self) -> T
+    where
+        T: Copy,
+    {
+        // SAFE: Since Unsafecell implements !Sync, we know no other thread is accessing this data
+        // We are additionally returning fresh data, thus no mutable refernece conflicts can occur
+        unsafe { *self.inner.get() }
+    }
+
+    pub fn set(&self, val: T) {
+        // SAFE: Since Unsafecell implements !Sync, we know no other thread is accessing this data
+        unsafe {
+            *self.inner.get() = val;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::Cell;
+
+    #[test]
+    fn works() {
+        let cell: Cell<i32> = Cell::new(69);
+        let inner = cell.get();
+        cell.set(10);
+        assert_ne!(cell.get(), inner);
+    }
 }
